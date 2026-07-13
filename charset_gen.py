b4e7af3a2371c5da4d1736c46852fd1e01c541c2
wyz-2015 <1209229723@qq.com>
2026-07-13 22:29:43 +0800

    追加检查字符是否被包含在字体中的功能。

--- charset_gen.py
+++ charset_gen.py
@@ -4,11 +4,12 @@ import json
 import argparse
 from pathlib import Path
 import sys
+from fontTools.ttLib import TTFont, TTCollection
 
 
-def get_char_set(lineData: dict) -> dict:
+def get_full_set(lineData: dict) -> set:
     """
-    输出内容格式：{"printable": "...", "non-printable": "..."}
+    从专门的文本数据文件中取得字符全集。
     """
     charSet_full = set()
 
@@ -18,13 +19,63 @@ def get_char_set(lineData: dict) -> dict:
                 for c in _str:
                     charSet_full.add(c)
 
-    # charSet_list = sorted(charSet_raw)
-    charSet_printable = set(c for c in charSet_full if (c.isprintable()))
+    return charSet_full
+
+
+def filter_printable(charSet: set[str]) -> dict:
+    """
+    区别可打印与非可打印字符。
+    输出内容格式：{"printable": {...}, "non-printable": {...}}
+    """
+
+    charSet_printable = set(c for c in charSet if (c.isprintable()))
 
     return {"printable":
-            "".join(sorted(charSet_printable)),
+            charSet_printable,
             "non-printable":
-            "".join(sorted(charSet_full.difference(charSet_printable)))}
+            charSet.difference(charSet_printable)}
+
+
+def char_in_ttf(c: str, ttf: TTFont) -> bool:
+    for table in ttf["cmap"].tables:
+        if (ord(c) in table.cmap):
+            return True
+    return False
+
+
+def char_in_ttc(c: str, ttc: TTCollection) -> tuple[bool, int]: #返回所在第一个字体的索引号
+    for fontIndex in range(len(ttc.fonts)):
+        for table in ttc.fonts[fontIndex]["cmap"].tables:
+            if (ord(c) in table.cmap):
+                return (True, fontIndex)
+    return (False, -1)
+
+
+def filter_font_support(charSet: set[str], fontPath: Path) -> dict:
+    """
+    区别字符在指定字体中是否有支持。
+    输出内容格式：{"inFont": {...}, "not_inFont": {...}}
+    """
+    result = {"inFont": set(), "not_inFont": set()}
+    
+    if (fontPath.suffix.lower() == ".ttc"):
+        ttc = TTCollection(fontPath)
+        for c in charSet:
+            (inTTC, ttcIndex) = char_in_ttc(c, ttc)
+            if (inTTC):
+                result["inFont"].add((c, ttcIndex))
+            else:
+                result["not_inFont"].add(c)
+
+    else:
+        ttf = TTFont(fontPath)
+        for c in charSet:
+            if (char_in_ttf(c, ttf)):
+                result["inFont"].add(c)
+            else:
+                result["not_inFont"].add(c)
+
+    return result
 
 
 def main():
@@ -32,20 +83,36 @@ def main():
     cmdParser.add_argument("jsonFilePath", type=Path, help="传入json文件的路径")
     cmdParser.add_argument("-o", "--out-txt", help="导出其中所有 可打印字符 到指定的纯文本文件中。所有字符将以json语言的形式打印在终端中。",
                            required=False, type=Path, default=None, dest="outTxtPath")
+    cmdParser.add_argument("-f", "--font", help="字体或字体集文件。用于判断字体是否包含了某字。", required=True,
+                           type=Path, dest="fontPath")
     
     args = cmdParser.parse_args()
+    print(args)
 
     jsonFile = open(args.jsonFilePath, "rt")
     lineData = json.load(jsonFile)
     jsonFile.close()
 
-    charSet_filtered = get_char_set(lineData)
+    ########################################
 
-    print(json.dumps(charSet_filtered, ensure_ascii=False, indent='\t'))
+    charSet_full = get_full_set(lineData)
+    f_printable = filter_printable(charSet_full)
+    f_inFont = filter_font_support(f_printable["printable"], args.fontPath)
+
+    print("可打印且字体支持：\n{0}\n可打印但字体不支持：\n{1}\n不可打印字符：{2}".format(
+        f_inFont["inFont"], f_inFont["not_inFont"], f_printable["non-printable"]))
 
     if (args.outTxtPath is not None):
+        _str = []
+        for i in f_inFont["inFont"]:
+            if (type(i) == str):
+                _str.append(i)
+            elif (type(i) == tuple):
+                _str.append(i[0])
+        _str.sort()
+
         txtFile = open(args.outTxtPath, "wt")
-        txtFile.write(charSet_filtered["printable"])
+        txtFile.write("".join(_str))
         txtFile.close()
 
 
